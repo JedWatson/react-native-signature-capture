297cafeed0f2a24b28f1231ca5e43c7a87d95c1e
Jed Watson <jed.watson@me.com>
2015-08-27 14:56:50 +1000

    Fixing mixed indentation

--- RSSignatureView.m
+++ RSSignatureView.m
@@ -8,10 +8,10 @@
 #define DEGREES_TO_RADIANS(x) (M_PI * (x) / 180.0)
 
 @implementation RSSignatureView {
-	CAShapeLayer *_border;
-	BOOL _loaded;
-	EAGLContext *_context;
-	UILabel *titleLabel;
+    CAShapeLayer *_border;
+    BOOL _loaded;
+    EAGLContext *_context;
+    UILabel *titleLabel;
 }
 
 @synthesize sign;
@@ -19,80 +19,80 @@
 
 -(instancetype) init
 {
-	if ((self = [super init])) {
-		_border = [CAShapeLayer layer];
-		_border.strokeColor = [UIColor blackColor].CGColor;
-		_border.fillColor = nil;
-		_border.lineDashPattern = @[@4, @2];
+    if ((self = [super init])) {
+        _border = [CAShapeLayer layer];
+        _border.strokeColor = [UIColor blackColor].CGColor;
+        _border.fillColor = nil;
+        _border.lineDashPattern = @[@4, @2];
 
-		[self.layer addSublayer:_border];
-	}
+        [self.layer addSublayer:_border];
+    }
 
-	return self;
+    return self;
 }
 
 -(void) layoutSubviews
 {
-	[super layoutSubviews];
-	if (!_loaded) {
-		_context = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
+    [super layoutSubviews];
+    if (!_loaded) {
+        _context = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
 
-		CGSize screen = self.bounds.size;
+        CGSize screen = self.bounds.size;
 
-		self.sign = [[PPSSignatureView alloc]
+        self.sign = [[PPSSignatureView alloc]
                      initWithFrame: CGRectMake(0, 0, screen.width, screen.height)
                      context: _context];
 
         [self addSubview:sign];
         
 
-		//Save button
-		UIButton *saveButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-		//[saveButton setTransform:CGAffineTransformMakeRotation(DEGREES_TO_RADIANS(90))];
-		//[saveButton setLineBreakMode:NSLineBreakByClipping];
-		[saveButton addTarget:self action:@selector(onSaveButtonPressed)
+        //Save button
+        UIButton *saveButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+        //[saveButton setTransform:CGAffineTransformMakeRotation(DEGREES_TO_RADIANS(90))];
+        //[saveButton setLineBreakMode:NSLineBreakByClipping];
+        [saveButton addTarget:self action:@selector(onSaveButtonPressed)
              forControlEvents:UIControlEventTouchUpInside];
         [saveButton setTitle:@"Save" forState:UIControlStateNormal];
         [saveButton.titleLabel setFont:[UIFont systemFontOfSize:22]];
 
-		saveButton.frame = CGRectMake(sign.bounds.size.width - buttonSize.width, 0, buttonSize.width, buttonSize.height);
-		[saveButton setBackgroundColor:[UIColor colorWithRed:255/255.f green:255/255.f blue:255/255.f alpha:1.f]];
-		[sign addSubview:saveButton];
+        saveButton.frame = CGRectMake(sign.bounds.size.width - buttonSize.width, 0, buttonSize.width, buttonSize.height);
+        [saveButton setBackgroundColor:[UIColor colorWithRed:255/255.f green:255/255.f blue:255/255.f alpha:1.f]];
+        [sign addSubview:saveButton];
 
-		//Clear button
-		UIButton *clearButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-		//[clearButton setTransform:CGAffineTransformMakeRotation(DEGREES_TO_RADIANS(90))];
-		//[clearButton setLineBreakMode:NSLineBreakByClipping];
-		[clearButton addTarget:self action:@selector(onClearButtonPressed)
+        //Clear button
+        UIButton *clearButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+        //[clearButton setTransform:CGAffineTransformMakeRotation(DEGREES_TO_RADIANS(90))];
+        //[clearButton setLineBreakMode:NSLineBreakByClipping];
+        [clearButton addTarget:self action:@selector(onClearButtonPressed)
               forControlEvents:UIControlEventTouchUpInside];
-		[clearButton setTitle:@"Reset" forState:UIControlStateNormal];
+        [clearButton setTitle:@"Reset" forState:UIControlStateNormal];
         [clearButton.titleLabel setFont:[UIFont systemFontOfSize:22]];
 
-		clearButton.frame = CGRectMake(0, 0, buttonSize.width, buttonSize.height);
-		[clearButton setBackgroundColor:[UIColor colorWithRed:255/255.f green:255/255.f blue:255/255.f alpha:1.f]];
-		[sign addSubview:clearButton];
+        clearButton.frame = CGRectMake(0, 0, buttonSize.width, buttonSize.height);
+        [clearButton setBackgroundColor:[UIColor colorWithRed:255/255.f green:255/255.f blue:255/255.f alpha:1.f]];
+        [sign addSubview:clearButton];
 
-	}
-	_loaded = true;
-	_border.path = [UIBezierPath bezierPathWithRect:self.bounds].CGPath;
-	_border.frame = self.bounds;
+    }
+    _loaded = true;
+    _border.path = [UIBezierPath bezierPathWithRect:self.bounds].CGPath;
+    _border.frame = self.bounds;
 }
 
 -(void) onSaveButtonPressed {
-	UIImage *signImage = [self.sign signatureImage];
+    UIImage *signImage = [self.sign signatureImage];
     NSError *error;
 
-	NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-	NSString *documentsDirectory = [paths firstObject];
-	NSString *tempPath = [documentsDirectory stringByAppendingFormat:@"/signature.png"];
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths firstObject];
+    NSString *tempPath = [documentsDirectory stringByAppendingFormat:@"/signature.png"];
 
-	// Remove the file if it already exists
-	if ([[NSFileManager defaultManager] fileExistsAtPath:tempPath]) {
-		[[NSFileManager defaultManager] removeItemAtPath:tempPath error:&error];
-		if (error) {
-			NSLog(@"Error: %@", error.debugDescription);
-		}
-	}
+    // Remove the file if it already exists
+    if ([[NSFileManager defaultManager] fileExistsAtPath:tempPath]) {
+        [[NSFileManager defaultManager] removeItemAtPath:tempPath error:&error];
+        if (error) {
+            NSLog(@"Error: %@", error.debugDescription);
+        }
+    }
 
     // Resize the image
     CGRect rect = CGRectMake(0, 0, 1024, 768);
@@ -103,17 +103,17 @@
     UIImage *resizedImage = UIGraphicsGetImageFromCurrentImageContext();
     UIGraphicsEndImageContext();
 
-	// Convert UIImage object into NSData (a wrapper for a stream of bytes) formatted according to PNG spec
-	NSData *imageData = UIImagePNGRepresentation(resizedImage);
-	BOOL isSuccess = [imageData writeToFile:tempPath atomically:YES];
-	if (isSuccess) {
-		NSString *base64Encoded = [imageData base64EncodedStringWithOptions:0];
-		[self.manager saveImage:tempPath withEncoded:base64Encoded];
-	}
+    // Convert UIImage object into NSData (a wrapper for a stream of bytes) formatted according to PNG spec
+    NSData *imageData = UIImagePNGRepresentation(resizedImage);
+    BOOL isSuccess = [imageData writeToFile:tempPath atomically:YES];
+    if (isSuccess) {
+        NSString *base64Encoded = [imageData base64EncodedStringWithOptions:0];
+        [self.manager saveImage:tempPath withEncoded:base64Encoded];
+    }
 }
 
 -(void) onClearButtonPressed {
-	[self.sign erase];
+    [self.sign erase];
 }
 
 @end
